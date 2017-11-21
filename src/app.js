5a47690f38e94eb12a6ae1ddff1f66201b2f146d
Init Built <lauwrentius.m@vmgstudio520.com>
2017-11-21 13:22:46 -0800

    Work on promises. Next: clicks and display infoWindow

--- src/app.js
+++ src/app.js
@@ -17,37 +17,19 @@ class Map{
     });
     this.markers = [];
   }
+
   displayMarkers(latlngArr){
-    Promise.all(latlngArr).then(arr=>{
-      //console.log("DISPLAY",addMarker);
-      // if(!markers) return;
-      console.log(arr);
-      for(var i=0;i<this.markers.length;i++){
-        var marker = arr.find(m=>{
-          console.log(this.markers[i], m);
-          return ((this.markers[i].getPosition().lat() === m.lat)&&
-            (this.markers[i].getPosition().lng() === m.lng))
-        });
-        console.log("FIND", marker);
-        if(marker)
-          this.markers[i].setVisible(true);
-        else
-          this.markers[i].setVisible(false);
-      }
-    });
-    // latlng.then(val=>{
-    //   console.log(latlng);
-    //   var marker = this.markers.find(m=>{
-    //     return ((m.getPosition().lat() === val.lat)&&
-    //       (m.getPosition().lng() === val.lng));
-    //   });
-    //
-    //   if(marker){
-    //     console.log("ASDASDASD", marker.getPosition().lat(),val.lat, marker.getPosition().lng(), val.lng, isHidden);
-    //     marker.setVisible(isHidden);
-    //   }
-    // });
+    for(var i=0;i<this.markers.length;i++){
+      var match = latlngArr.find(m=>{
+        return this.markers[i].getPosition().equals(new google.maps.LatLng(m));
+      });
+      if(match)
+        this.markers[i].setVisible(true);
+      else
+        this.markers[i].setVisible(false);
+    }
   }
+
   addMarker(latlng){
     this.markers.push(new google.maps.Marker({
       map: this.map,
@@ -80,6 +62,7 @@ class PoI{
         });
     });
   }
+
   get latlng(){
     return this.venue.then(val=>{
       return {lat:val.location.lat,lng:val.location.lng};
@@ -87,20 +70,15 @@ class PoI{
       console.log("FAIL",val);
       return null;
     });
-
-    // return this.venue
-    // return this.venue.then({lat: this.venue.location.lat, lng: this.venue.location.lat});
-    //return this.latlng;
   }
-  // set marker(marker){
-  //   console.log("SET MARKER", marker);
-  //   this.marker = marker;
-  // }
 }
 
 
 var ViewModel = function() {
     this.poi_arr = ko.observableArray();
+    this.textFilter = ko.observable("");
+    this.currentItem = ko.observable(null);
+
     google.maps.event.addDomListener(window, 'load', ()=>{
       this.map = new Map();
       this.InitPlaces();
@@ -118,59 +96,30 @@ var ViewModel = function() {
       }
     }
 
-// console.log( this.poi_arr );
-    this.item_click = function(evt){
-      console.log(evt);
+    this.item_click = item => {
+      this.currentItem(item);
     }
-    this.textFilter = ko.observable("");
+    this.isCurrentItem = item=>{
+      // console.log(item,this.currentItem());
+      if(item === this.currentItem())
+        return true;
+      return false;
+    };
+
     this.poi_filterred = ko.computed(()=>{
-      console.log("NRE");
       var filtered = this.poi_arr.slice().filter(poi => {
         return (new RegExp(this.textFilter(),'i')).test(poi.name);
       });
-      if(this.map)
-        this.map.displayMarkers(filtered);
+      if(this.map){
+        var latlngArr = filtered.map(i=>{return i.latlng});
+        Promise.all(latlngArr).then(val=>{
+          // console.log("ALL",x);
+          this.map.displayMarkers(val);
+        })
+      }
       return filtered;
     }, this);
 };
 
-// function initMarker(map){
-//   for(var i=0; i<poi_data.length; i++){
-//     //console.log
-//     poi_data[i].marker = new google.maps.Marker({
-//       map: map,
-//       draggable: false,
-//       animation: google.maps.Animation.DROP,
-//       position: {lat: poi_data[i].lat, lng: poi_data[i].lng}
-//     });
-//   }
-// }
-//var map;
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: 47.6090857175388, lng: -122.18652458190917},
-//     zoom: 14
-//   });
 
-  // google.maps.event.addDomListener(window, 'load', function(){
-  //   console.log("INIT");
-  //   //initMarker(map);
-  // });
-  // map.addListener('dragend', function() {
-  //   console.log(map.getCenter().lat(),map.getCenter().lng());
-  // });
-
-  //return map;
-//}
-// window.onload = function(){
-//   console.log("WINDOW LOAD");
-//   var map = initMap();
-//   initMarker(map);
-// }
 ko.applyBindings(new ViewModel());
-
-
-
-
-// initMap();
-// This makes Knockout get to work
